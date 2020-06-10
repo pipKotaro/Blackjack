537cef3783762fafe485f511a39d0bf175181423
pipKotaro <42577919+pipKotaro@users.noreply.github.com>
2020-06-10 15:45:58 +0900

    Delete Player.java

--- BJ/Player.java
+++
@@ -1,9 +0,0 @@
-class Player extends Person{
-
-    public Player(String name,Card cards){
-        super(name,cards);
-        System.out.println("(Player)"+name+"の最初の合計:"+getScore());
-    }
-
-    
-}
